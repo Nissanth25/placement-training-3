23fe2b228d8dd8e6c4651ab0e37752d64c1aff8c
Nissanth <147823208+Nissanth25@users.noreply.github.com>
2025-12-21 20:07:31 +0530

    Create Determine Color of a Chessboard Square.java

---
+++ 21.12.2025/Determine Color of a Chessboard Square.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean squareIsWhite(String coordinates) {
+        char column = coordinates.charAt(0);
+        int row = coordinates.charAt(1) - '0';
+        int columnIdx = column - 'a' + 1;
+        return (columnIdx + row) % 2 != 0;
+    }
+}
